ae04271f93c7bef4cbc4d00057ea51311facdf91
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-06 18:27:03 +0200

    imported axios and added album cards

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="album">
+    <img :src="albumCard.poster" :alt="albumCard.title" />
+    <h3 class="ttu">{{ albumCard.title }}</h3>
+    <div class="author">{{ albumCard.author }}</div>
+    <div class="year">{{ albumCard.year }}</div>
+    <div class="genre">{{ albumCard.genre }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: ["albumCard"],
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/vars";
+@import "../styles/mixins";
+
+.album {
+  @include df("vertical");
+  flex-direction: column;
+  padding: 1rem;
+  height: 380px;
+  background: $brand-color;
+  h3 {
+    font-size: 20px;
+    text-align: center;
+    color: $text-primary;
+    margin: 1.5rem 0 1rem;
+  }
+  .author {
+    font-size: 20px;
+  }
+  .author,
+  .year {
+    margin-bottom: 0.5rem;
+    color: $text-secondary;
+  }
+  .genre {
+    color: $text-primary;
+  }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,12 +1,45 @@
 <template>
   <section class="main-content">
-    MAIN CONTENT HERE
+    <div v-if="!loading" class="cards">
+      <div v-for="(album, index) in albumList" :key="index" class="album">
+        <AlbumCard :albumCard="album" />
+      </div>
+    </div>
+    <div v-else class="loading">Loading...</div>
   </section>
 </template>
 
 <script>
+import axios from "axios";
+import AlbumCard from "@/components/AlbumCard.vue";
 export default {
   name: "MainContent",
+  components: {
+    AlbumCard,
+  },
+  data() {
+    return {
+      apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
+      albumList: [],
+      loading: true,
+    };
+  },
+  created() {
+    this.getAlbums();
+  },
+  methods: {
+    getAlbums() {
+      axios
+        .get(this.apiURL)
+        .then((res) => {
+          this.albumList = res.data.response;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log("ERROR", err);
+        });
+    },
+  },
 };
 </script>
 
@@ -18,5 +51,14 @@ export default {
   @include df();
   height: calc(100vh - 70px);
   background: $brand-background;
+  .cards {
+    @include df();
+    flex-wrap: wrap;
+    margin-top: 1rem;
+    .album {
+      flex-basis: calc(100% / 8 - 2rem);
+      margin: 0.5rem;
+    }
+  }
 }
 </style>
