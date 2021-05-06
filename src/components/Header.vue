c58db1280bf937a33f98684179706c8aadffb61b
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-06 17:07:00 +0200

    initialized project and added base structure

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="main-header">
+    <img src="../assets/logo.png" alt="spotify-logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/vars";
+@import "../styles/mixins";
+.main-header {
+  @include df("vertical");
+  height: 70px;
+  background: $brand-color;
+  img {
+    height: 50px;
+    margin-left: 0.5rem;
+  }
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="main-content">
+    MAIN CONTENT HERE
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MainContent",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/vars";
+@import "../styles/mixins";
+
+.main-content {
+  @include df();
+  height: calc(100vh - 70px);
+  background: $brand-background;
+}
+</style>
